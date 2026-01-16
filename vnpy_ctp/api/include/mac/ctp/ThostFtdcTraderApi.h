a4c137f1d7f41198c35fa6c965ef2d04c33a988b
HanYuanDao <hanzhe.jason@gmail.com>
2026-01-16 13:30:32 +0800

    更新Api

--- vnpy_ctp/api/include/mac/ctp/ThostFtdcTraderApi.h
+++ vnpy_ctp/api/include/mac/ctp/ThostFtdcTraderApi.h
@@ -620,7 +620,7 @@ public:
 	virtual int SubmitWechatUserSystemInfo(CThostFtdcWechatUserSystemInfoField *pUserSystemInfo) = 0;
 
 	///用户登录请求
-	virtual int ReqUserLogin(CThostFtdcReqUserLoginField *pReqUserLoginField, int nRequestID) = 0;
+	virtual int ReqUserLogin(CThostFtdcReqUserLoginField *pReqUserLoginField, int nRequestID, TThostFtdcSystemInfoLenType length, TThostFtdcClientSystemInfoType systemInfo) = 0;
 
 	///登出请求
 	virtual int ReqUserLogout(CThostFtdcUserLogoutField *pUserLogout, int nRequestID) = 0;
